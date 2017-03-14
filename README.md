844e0eae87c4bfc4af47aab2a99bdac0afd05323
Ranga Pathmasiri <ranga@wayforward.lk>
2017-03-14 19:26:44 +0530

    Add

---
+++ README.md
@@ -0,0 +1,19 @@
+node-mysql-json-server
+===========
+
+Testing setting up a json-server with Node, Restify and node-mysql.
+
+You can make a dicitonary, so save your translations.
+
+Restify can very easily be replaced with Express, maybe just by changing the require-module to use Express.
+See here the reason why you might want to use Restify: http://mcavage.github.com/node-restify/
+
+This json-server saves notes to a database.
+
+I also did a similar test with mongo: https://github.com/frodefi/node-mongodb-json-server
+
+I will next do a test using mongodb and socket.io, then I will also include authentication.
+If you don't want to use socket.io, you can take out the authentication part and put it in here.
+See https://github.com/frodefi/node-mongodb-socketio-server
+
+Please feel free to use this code as you like :)
---
+++ db.js
@@ -0,0 +1,111 @@
+// Database related
+'use strict';
+
+var mysql   = require('mysql')
+  , connection = mysql.createConnection({
+      host     : "bk-prod-us1.cd2junihlkms.us-east-1.rds.amazonaws.com",
+      user     : "ihann0syfqqekwz",
+      password : "aOfLAEu63bq0f6VND48jVe",
+      database : "backands148754424wgfimxv2"
+    });
+
+connection.connect(function(err) {
+  // connected! (unless `err` is set)
+  if (err) {
+    console.log("DB conn error...");
+  } else {
+    console.log("DB connected :)");
+  }
+});
+
+
+function handleDisconnect(connection) {
+  connection.on('error', function(err) {
+    if (!err.fatal) {
+      return;
+    }
+
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      throw err;
+    }
+
+    console.log('Re-connecting lost connection: ' + err.stack);
+
+    connection = mysql.createConnection(connection.config);
+    handleDisconnect(connection);
+    connection.connect();
+  });
+}
+
+handleDisconnect(connection);
+
+// Testing with a table representing a simple dictionary:
+// CREATE TABLE Translations (
+//    Id int UNSIGNED NOT NULL AUTO_INCREMENT,
+//    Dictionary mediumint UNSIGNED,
+//    UserId mediumint UNSIGNED, // When a user creates his own or modifies a default translation
+//    OriginalTranslationId int UNSIGNED, // then these 2 columns are used
+//    FromWord tinytext,
+//    FromDescription text,
+//    ToWord tinytext,
+//    ToDescription text,
+//    LastChanged timestamp,
+//    PRIMARY KEY(Id)
+// )
+//
+
+exports.getTranslationsByDictionary = function (data, callback) {
+  var queryData = [
+    { UserId              : data[0] },
+    { Dictionary          : data[1] }
+  ];
+  var query = connection.query('SELECT * FROM Translations WHERE (UserId = 0 OR ?) AND ?;', queryData, callback);
+  console.log(query.sql);
+};
+
+exports.getTranslationsByDate = function (data, callback) {
+  var queryData = [
+    { UserId              : data[0] },
+    { Dictionary          : data[1] },
+    data[2] // LastChanged
+  ];
+  var query = connection.query('SELECT * FROM Translations WHERE (UserId = 0 OR ?) AND ? AND LastChanged > ?;', queryData, callback);
+  console.log(query.sql);
+};
+
+exports.createTranslation = function (data, callback) {
+  var queryData = {
+    Dictionary            : data[0],
+    UserId                : data[1],
+    FromWord              : data[2],
+    FromDescription       : data[3],
+    ToWord                : data[4],
+    ToDescription         : data[5],
+    OriginalTranslationId : data[6]
+  }
+  connection.query('INSERT INTO Translations SET ?', queryData, callback);
+};
+
+exports.updateTranslation = function (data, callback) {
+  var queryData = [
+    {
+      FromWord            : data[0],
+      FromDescription     : data[1],
+      ToWord              : data[2],
+      ToDescription       : data[3]
+    },
+    { UserId              : data[4] },
+    { Id                  : data[5] }
+  ];
+  var query = connection.query('UPDATE Translations SET ? WHERE ? AND ?', queryData, callback);
+  console.log(query.sql);
+};
+
+exports.deleteTranslation = function (data, callback) {
+  var queryData = [
+    { UserId              : data[0] },
+    { Id                  : data[1] }
+  ];
+  var query = connection.query('DELETE FROM Translations WHERE ? AND ?;', queryData, callback);
+  console.log(query.sql);
+};
